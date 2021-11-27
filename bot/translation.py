316029bd0993479b6f50eb536125285b37525688
Itsmeadhi <91237261+Itsmeadhi@users.noreply.github.com>
2021-11-27 10:11:46 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,7 +5,7 @@
 class Translation(object):
     
     START_TEXT = """<b><u>Hey {}!!</b></u>
-<i>Am SHIBHUKUTTAN BOT... ⚡️
+<i>IAm SHIBHUKUTTAN BOT... ⚡️
 
 I AM ONLY MADE FOR MOVIE STATION GROUP SO YOU CAN'T ADD ME TO ANY GROUP 😜
 
@@ -14,20 +14,12 @@ For More Details Click Help Button Below..
 </i>"""    
     
     HELP_TEXT = """
-<b><i><u>ഡാ ഇവിടെ എന്താടാ പണി 🧐?</u></i></b>
+<b><i><u>വിളച്ചിൽ എടുക്കരുത് കേട്ടോ😤?</u></i></b>
 
 <i>
 സിനിമ കിട്ടിയേകിൽ എടുത്തോണ്ട് പോ വെറുതെ എന്റെ കൈക്ക് പണിയുണ്ടാകരുത് 😤
-@moviestation
+@joinmoviestationgroup
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/@Adhi140141>@Adhi140141</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
-"""
+   
