9c7c7527270e602e327ace1c37d492b639ff4aae
Itsmeadhi <91237261+Itsmeadhi@users.noreply.github.com>
2021-11-27 10:19:27 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -22,4 +22,13 @@ For More Details Click Help Button Below..
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    
+<b>➥ Creator</b> : <b><i><a href="https://t.me/@Adhi140141>@Adhi140141</a></i></b>
+
+<b>➥ Language</b> : <code>Python3</code>
+
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+"""
    
