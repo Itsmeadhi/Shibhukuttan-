f1594e38d794a879ef5b9fd654a82973798e5d60
Itsmeadhi <91237261+Itsmeadhi@users.noreply.github.com>
2021-11-28 07:26:42 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    '🔰JOIN OUR GROUP🔰', url="@joinmoviestationgroup"
+                                    '🔰JOIN OUR GROUP🔰', url='https://t.me/joinmoviestationgroup'
                                 )
                         ]
                     ]
@@ -48,7 +48,7 @@ async def start(bot, update):
 
     buttons = [[
         InlineKeyboardButton('MY FATHER', url='@Adhi140141'),
-        InlineKeyboardButton(MY GROUP 🧾', url ='@joinmoviestationgroup'
+        InlineKeyboardButton(MY GROUP 🧾', url ='https://t.me/joinmoviestationgroup')
     ],[
         InlineKeyboardButton('MY WHATSAPP GROUP', url='https://chat.whatsapp.com/JT3cKDhh4XRHOqZE42BmiJ')
     ],[
